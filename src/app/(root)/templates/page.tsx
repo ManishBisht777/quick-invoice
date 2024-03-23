8982517db40d4c2d302dcb8bd7c83f1236cac996
ManishBisht7 <manishbisht9711@gmail.com>
2024-03-24 01:19:15 +0530

    feat: ui work

--- src/app/(root)/templates/page.tsx
+++ src/app/(root)/templates/page.tsx
@@ -1,17 +1,49 @@
+import { Button, buttonVariants } from "@/components/ui/button";
 import { AllTemplates } from "@/lib/templates/util";
+import { cn } from "@/lib/utils";
+import { Star } from "lucide-react";
 import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="grid grid-cols-3 gap-6 min-h-screen justify-center">
-      {Object.keys(AllTemplates).map((key) => {
-        const templateImage = AllTemplates[key].image;
-        return (
-          <Link href={`/editor/${key}`} key={key}>
-            <img src={templateImage} alt={key} fill className="object-cover" />
+    <div>
+      <div className="flex gap-4">
+        <div className="px-6 py-8 border rounded-lg w-full flex justify-between items-center">
+          <div>
+            <h2 className="text-xl font-semibold">Templates</h2>
+            <p className="text-muted-foreground text-sm">
+              Explore a wide range of templates for base of your invoice
+            </p>
+          </div>
+          <Link
+            className={cn(buttonVariants(), "flex gap-2")}
+            href="https://github.com/manishbisht777/quick-invoice/"
+          >
+            <Star size={16} />
+            Star
           </Link>
-        );
-      })}
+        </div>
+        {/* <div className="px-6 py-8 border rounded-lg bg-grid-2 w-1/5 space-y-1">
+          <h4 className="text-xl font-semibold">$5000+</h4>
+          <p className="text-muted-foreground text-sm">Splitted so far</p>
+        </div> */}
+      </div>
+
+      <div className="grid grid-cols-3 gap-6 min-h-screen justify-center mt-6">
+        {Object.keys(AllTemplates).map((key) => {
+          const templateImage = AllTemplates[key].image;
+          return (
+            <Link href={`/editor/${key}`} key={key}>
+              <img
+                src={templateImage}
+                alt={key}
+                fill
+                className="object-cover"
+              />
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,5 +1,6 @@
 import { Flower } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface NavbarProps {}
 
@@ -9,10 +10,12 @@ export default function Navbar({}: NavbarProps) {
       <div className="border w-fit p-2">
         <Image src="/icons/menu.svg" width={35} height={35} alt="Menu" />
       </div>
-      <div className="flex-1 border-t border-b border-r flex justify-center items-center gap-2">
-        Snappy
-        <Flower size={24} />
-        Invoice
+      <div className="flex-1 border-t border-b border-r flex justify-center items-center">
+        <Link href="/" className="flex gap-2">
+          Snappy
+          <Flower size={24} />
+          Invoice
+        </Link>
       </div>
     </div>
   );
