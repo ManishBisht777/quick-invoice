5a8514c995bb53e135e5c8be2f8a59a106a36958
ManishBisht7 <manishbisht9711@gmail.com>
2024-03-30 14:44:05 +0530

    feat: google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <Toaster />
+      <GoogleAnalytics gaId="G-NF2CY20H95" />
     </html>
   );
 }
