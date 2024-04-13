ff8e9a1082c1fd140fb388c0b725a45b52566861
ManishBisht7 <manishbisht9711@gmail.com>
2024-04-13 14:00:03 +0530

    feat: invoice details

---
+++ src/app/(root)/dashboard/invoice/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
+}
--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,8 +1,11 @@
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { StatusBadge } from "@/components/ui/status-badge";
 import { db } from "@/lib/db";
 import { getSession } from "@/lib/session";
+import { AllTemplates } from "@/lib/templates/util";
 import { ChevronRight, MoveRight } from "lucide-react";
 import CellAction from "./cell-action";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 type Props = {};
 
@@ -34,41 +37,60 @@ export default async function page({}: Props) {
       </div>
 
       <div className="grid md:grid-cols-3 grid-cols-2 gap-5">
-        {invoices.map((invoice) => (
-          <div
-            key={invoice.id}
-            className="p-6 border border-slate-300 rounded-lg font-serif h-60 flex flex-col justify-between cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out group"
-          >
-            <div className="flex justify-between items-center">
-              <StatusBadge
-                variant={
-                  invoice.status === "Draft"
-                    ? "draft"
-                    : invoice.status === "Paid"
-                    ? "paid"
-                    : invoice.status === "Overdue"
-                    ? "overdue"
-                    : invoice.status === "Sent"
-                    ? "sent"
-                    : "draft"
-                }
-              >
-                {invoice.status}
-              </StatusBadge>
-              <CellAction data={invoice} />
-            </div>
-            <div>
-              <div className="flex gap-4 items-center">
-                <h2 className="text-2xl text-slate-800">{invoice.name}</h2>
-                <MoveRight className="text-hot-orange group-hover:translate-x-1 transition-all" />
+        {invoices.map((invoice) => {
+          const Template = AllTemplates[invoice.template]?.component;
+
+          return (
+            <div
+              key={invoice.id}
+              className="p-6 border border-slate-300 rounded-lg font-serif h-60 flex flex-col justify-between cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out group"
+            >
+              <div className="flex justify-between items-center">
+                <StatusBadge
+                  variant={
+                    invoice.status === "Draft"
+                      ? "draft"
+                      : invoice.status === "Paid"
+                      ? "paid"
+                      : invoice.status === "Overdue"
+                      ? "overdue"
+                      : invoice.status === "Sent"
+                      ? "sent"
+                      : "draft"
+                  }
+                >
+                  {invoice.status}
+                </StatusBadge>
+                <CellAction data={invoice} />
               </div>
-              <p className="text-muted-foreground text-sm">
-                {invoice.totalAmount} -{" "}
-                {new Date(invoice.createdAt).toLocaleDateString()}
-              </p>
+              <div>
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <div className="flex gap-4 items-center">
+                      <h2 className="text-2xl text-slate-800">
+                        {invoice.name}
+                      </h2>
+                      <MoveRight className="text-hot-orange group-hover:translate-x-1 transition-all" />
+                    </div>
+                  </SheetTrigger>
+                  <SheetContent className="w-[37rem]">
+                    <p className="text-2xl font-semibold font-serif">
+                      Invoice Details
+                    </p>
+                    <ScrollArea className="h-[96vh] mt-8">
+                      <Template initialValue={JSON.parse(invoice.content)} />
+                    </ScrollArea>
+                  </SheetContent>
+                </Sheet>
+
+                <p className="text-muted-foreground text-sm">
+                  {invoice.totalAmount} -{" "}
+                  {new Date(invoice.createdAt).toLocaleDateString()}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       {/* <DataTable columns={invoiceColumns} data={invoices} /> */}
