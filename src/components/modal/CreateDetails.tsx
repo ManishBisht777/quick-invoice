6773ee85592211db87729562741c22d4dcdf0890
ManishBisht7 <manishbisht9711@gmail.com>
2024-04-13 22:37:03 +0530

    fix: create details styles

--- src/components/modal/CreateDetails.tsx
+++ src/components/modal/CreateDetails.tsx
@@ -20,7 +20,7 @@ export default function CreateDetails({}: CreateDetailsProps) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <div className="flex border-dashed border-2 items-center justify-center rounded-md flex-col cursor-pointer h-full gap-1 capitalize hover:bg-slate-50/40">
+        <div className="min-h-40 flex border-dashed border-2 items-center justify-center rounded-md flex-col cursor-pointer h-full gap-1 capitalize hover:bg-slate-50/40">
           <Plus size={30} />
           Create new details
         </div>
