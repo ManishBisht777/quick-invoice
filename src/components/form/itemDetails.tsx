daca37fbdf04d51dcf4d8d5a7b702eb9824fef0e
ManishBisht7 <manishbisht9711@gmail.com>
2024-04-20 01:34:43 +0530

    feat: add subtype

--- src/components/form/itemDetails.tsx
+++ src/components/form/itemDetails.tsx
@@ -11,9 +11,13 @@ import { Input } from "@/components/ui/input";
 import { items } from "@/types/template";
 import { TrashIcon } from "lucide-react";
 import { Label } from "../ui/label";
+import { UseFormReturn } from "react-hook-form";
+import { z } from "zod";
+import { templatePropsSchema } from "@/types/formSchema";
+import { WorkType } from "@/enum/work";
 
 interface ItemDetailsProps {
-  form: any;
+  form: UseFormReturn<z.infer<typeof templatePropsSchema>>;
   fields: items[];
   append: any;
   remove: any;
@@ -55,49 +59,78 @@ export default function ItemDetails({
                   </FormItem>
                 )}
               />
-              <FormField
-                control={form.control}
-                name={`items.${index}.quantity`}
-                render={({ field }) => (
-                  <FormItem className="col-span-1">
-                    <Label htmlFor={`items.${index}.quantity`}>Qty</Label>
-                    <FormControl>
-                      <Input type="number" placeholder="3" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name={`items.${index}.price`}
-                render={({ field }) => (
-                  <FormItem className="col-span-1">
-                    <Label htmlFor={`items.${index}.price`}>Price</Label>
-                    <FormControl>
-                      <Input type="number" placeholder="1500" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <FormField
-              control={form.control}
-              name={`items.${index}.description`}
-              render={({ field }) => (
-                <FormItem>
-                  <Label htmlFor={`items.${index}.description`}>
-                    Description
-                  </Label>
-                  <FormControl>
-                    <Input placeholder="Design and 3 iteration" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
+              {form.getValues("invoiceDetails.workType") ===
+                WorkType.QUANTITY && (
+                <FormField
+                  control={form.control}
+                  name={`items.${index}.quantity`}
+                  render={({ field }) => (
+                    <FormItem className="col-span-1">
+                      <Label htmlFor={`items.${index}.quantity`}>Qty</Label>
+                      <FormControl>
+                        <Input type="number" placeholder="3" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
               )}
-            />
+
+              {(form.getValues("invoiceDetails.workType") ===
+                WorkType.QUANTITY ||
+                form.getValues("invoiceDetails.workType") ===
+                  WorkType.FIXED) && (
+                <FormField
+                  control={form.control}
+                  name={`items.${index}.price`}
+                  render={({ field }) => (
+                    <FormItem className="col-span-1">
+                      <Label htmlFor={`items.${index}.price`}>Price</Label>
+                      <FormControl>
+                        <Input type="number" placeholder="1500" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              )}
+
+              <FormField
+                control={form.control}
+                name={`items.${index}.description`}
+                render={({ field }) => (
+                  <FormItem className="col-span-4">
+                    <Label htmlFor={`items.${index}.description`}>
+                      Description
+                    </Label>
+                    <FormControl>
+                      <Input placeholder="Design and 3 iteration" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {form.getValues("invoiceDetails.workType") ===
+                WorkType.HOURLY && (
+                <FormField
+                  control={form.control}
+                  name={`items.${index}.hours`}
+                  defaultValue={"0"}
+                  render={({ field }) => (
+                    <FormItem className="col-span-2">
+                      <Label htmlFor={`items.${index}.hours`}>
+                        Hours worked
+                      </Label>
+                      <FormControl>
+                        <Input placeholder="HH:MM" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              )}
+            </div>
           </div>
         ))}
       </div>
