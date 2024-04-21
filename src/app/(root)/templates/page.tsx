128b8731f6319e644e7114fcf78b0cadde32b6c6
ManishBisht7 <manishbisht9711@gmail.com>
2024-04-21 23:50:38 +0530

    deps: add drawer

--- src/app/(root)/templates/page.tsx
+++ src/app/(root)/templates/page.tsx
@@ -1,32 +1,25 @@
-import { Button, buttonVariants } from "@/components/ui/button";
 import { AllTemplates } from "@/lib/templates/util";
-import { cn } from "@/lib/utils";
-import { Star } from "lucide-react";
 import Link from "next/link";
 
 export default function Page() {
   return (
     <div>
-      <div className="flex gap-4">
-        <div className="px-6 py-8 border rounded-lg w-full flex justify-between items-center">
-          <div>
-            <h2 className="text-xl font-semibold">Templates</h2>
-            <p className="text-muted-foreground text-sm">
-              Explore a wide range of templates for base of your invoice
-            </p>
-          </div>
-          <Link
-            className={cn(buttonVariants(), "flex gap-2")}
-            href="https://github.com/manishbisht777/quick-invoice/"
-          >
-            <Star size={16} />
-            Star
-          </Link>
+      <div className="px-6 py-8 border rounded-lg w-full flex justify-between items-center">
+        <div>
+          <h2 className="text-2xl font-semibold flex gap-1 items-center">
+            <span className="text-hot-orange">Templates</span>
+          </h2>
+          <p className="text-muted-foreground text-sm">
+            Explore a wide range of templates for base of your invoice
+          </p>
         </div>
-        {/* <div className="px-6 py-8 border rounded-lg bg-grid-2 w-1/5 space-y-1">
-          <h4 className="text-xl font-semibold">$5000+</h4>
-          <p className="text-muted-foreground text-sm">Splitted so far</p>
-        </div> */}
+        {/* <Link
+          className={cn(buttonVariants(), "flex gap-2")}
+          href="https://github.com/manishbisht777/quick-invoice/"
+        >
+          <Star size={16} />
+          Star
+        </Link> */}
       </div>
 
       <div className="grid grid-cols-3 gap-6 min-h-screen justify-center mt-6">
