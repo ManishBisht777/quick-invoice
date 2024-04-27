1a998895ca04dfdcfaaef0cf98146351609b5b9d
ManishBisht7 <manishbisht9711@gmail.com>
2024-04-27 17:13:04 +0530

    deps: rm pptr core, aws lamda and pw core

--- src/server/generatePdf.ts
+++ src/server/generatePdf.ts
@@ -1,10 +1,4 @@
 import { NextRequest } from "next/server";
-import chromium from "chrome-aws-lambda";
-import playwright from "playwright-core";
-import edgeChromium from "chrome-aws-lambda";
-import puppeteer from "puppeteer-core";
-
-const LOCAL_CHROME_EXECUTABLE = "C:/Program Files/Google/Chrome/Applications";
 
 export const generatePdf = async (req: NextRequest) => {
   const data = await req.json();
@@ -12,37 +6,10 @@ export const generatePdf = async (req: NextRequest) => {
   try {
     const templateId = data.templateId;
 
-    // const browser = await chromium.puppeteer.launch({
-    //   args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-    //   defaultViewport: chromium.defaultViewport,
-    //   executablePath: await chromium.executablePath,
-    //   headless: true,
-    //   ignoreHTTPSErrors: true,
-    // });
-
-    // let browser;
-    // const puppeteer = require("puppeteer");
-    // browser = await puppeteer.launch({
-    //   args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    //   headless: true,
-    // });
-
-    // const browser = await playwright.chromium.launch({
-    //   args: chromium.args,
-    //   headless:
-    //     process.env.NODE_ENV === "production" ? chromium.headless : true,
-    //   executablePath:
-    //     process.env.NODE_ENV === "production"
-    //       ? await chromium.executablePath
-    //       : "",
-    // });
-
-    const executablePath =
-      (await edgeChromium.executablePath) || LOCAL_CHROME_EXECUTABLE;
+    const puppeteer = require("puppeteer");
 
     const browser = await puppeteer.launch({
-      executablePath,
-      args: edgeChromium.args,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
       headless: false,
     });
 
@@ -63,30 +30,19 @@ export const generatePdf = async (req: NextRequest) => {
     console.log("Page loaded");
 
     const pdf = await page.screenshot({
-      // format: "A4",
-      // printBackground: true,
+      format: "A4",
+      printBackground: true,
       type: "png",
     });
 
     await browser.close();
 
-    // const pdfBlob = new Blob([pdf], { type: "application/pdf" });
+    const pdfBlob = new Blob([pdf], { type: "application/pdf" });
 
-    // const response = new Response(pdfBlob, {
-    //   headers: {
-    //     "Content-Type": "application/pdf",
-    //     "Content-Disposition": "inline; filename=invoice.pdf",
-    //   },
-    //   status: 200,
-    // });
-
-    console.log(pdf);
-
-    const imageBlob = new Blob([pdf], { type: "image/png" });
-
-    const response = new Response(imageBlob, {
+    const response = new Response(pdfBlob, {
       headers: {
-        "Content-Type": "image/png",
+        "Content-Type": "application/pdf",
+        "Content-Disposition": "inline; filename=invoice.pdf",
       },
       status: 200,
     });
